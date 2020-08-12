8e9d6176cde9ba98af5ac541d76746ee92d49bd8
megan <meggsnbacon777@gmail.com>
2020-08-12 06:08:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,10 +7,10 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
 end
-
+index=1
 def list_dwarves(array)
   array.each_with_index do |item,index|
-    p "#{index}:#{item}"
+    p "#{index}.#{item}"
     
     
   end
