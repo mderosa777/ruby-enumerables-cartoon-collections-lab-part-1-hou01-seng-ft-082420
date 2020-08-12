23d30842d268da0885e2383f0b65d86df4b27a4c
megan <meggsnbacon777@gmail.com>
2020-08-12 05:35:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each do |each_with_index|
-    puts each_with_index
+  array.each_with_index
   end
   # Use `each_with_index` to enumerate over the provided array
   #
